a361cc8d7e42f7c5db7445c2d185dfe562e1cafd
Alex Aylwin <alexaylwin@gmail.com>
2022-07-15 20:17:11 -0400

    Clean up UI

--- src/components/pick.tsx
+++ src/components/pick.tsx
@@ -10,16 +10,19 @@ export const Pick = (props: { num: number, odds: Odds[], handlePick: (team: numb
   //TODO: Calculate team % odds
   // Each team - team picks / total
   const total = Object.values(odds).reduce( (total:number, {count}) => total + count, 0);
-  const percentageOdds = odds.map( (o) => { return { teamName: o.teamName, percent: (o.count/total).toFixed(2) } });
+  const percentageOdds = odds.map( 
+    (o) => { 
+      return { teamName: o.teamName, percent: Math.round(((o.count/total)*100)) } 
+    }
+  );
   
   return (
-<div className="pick mb-5 text-3xl flex flex-col border-y py-2 my-4">
+<div className={ (donePick ? "bg-gray-800" : "") + " pick mb-5 text-3xl flex flex-col border-y py-2 my-4"}>
           <div className="flex flex-row justify-center">
-            <div className="pr-5 self-center">Pick {num}:</div>
-            <Roller odds={odds} handlePick={handlePick}></Roller>
+            <Roller num={num} odds={odds} handlePick={(n:number) => { setDonePick(true); handlePick(n) }}></Roller>
           </div>
-          <div className={"text-base flex flex-row justify-center mt-2"}>
-            { percentageOdds.map((v, i) => (<span key={i}>{v.teamName + ' - ' + v.percent + '%'},&nbsp;</span>)) }
+          <div className={(donePick ? "hidden" : "") + " text-base flex flex-row justify-center mt-2"}>
+            |&nbsp;{ percentageOdds.map((v, i) => (<span key={i}>{v.teamName + ' - ' + v.percent + '%'} |&nbsp;</span>)) }
           </div>
         </div>    
   )
--- src/components/roller.tsx
+++ src/components/roller.tsx
@@ -5,10 +5,12 @@ import { defaultOdds, getTeam, Odds } from "../models/odds";
 
 export const Roller = (props: {
   odds: Odds[],
-  handlePick: (pick: number) => void
+  handlePick: (pick: number) => void,
+  num: number
 }) => {
   const [cn, setCn] = useState('');
   const [showButton, setShowButton] = useState(true);
+  const {odds, num, handlePick} = props;
 
   const rnd = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
@@ -34,13 +36,14 @@ export const Roller = (props: {
   }
   
   return (
-    <div className="roller w-auto">
+    <div className="roller w-auto flex flex-row justify-center">
+      <div className={(!showButton ? '' : 'hidden') + " pr-5 self-center"}>Pick {num}:</div>
       <button 
         className={ (showButton ? '' : 'hidden') + ` rounded-xl px-1 py-1 border-blue-300 border-4 text-2xl text-blue-300
           hover:bg-black hover:bg-opacity-25 hover:text-blue-100 hover:border-blue-100
           transition duration-300`
         }
-        onClick={() => startRoll() }>DRAW</button>
+        onClick={() => startRoll() }>DRAW PICK #{num} </button>
 
         <div className ={ (showButton ? 'hidden' : '')} >{cn}</div>
     </div>  
